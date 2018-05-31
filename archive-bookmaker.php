d5ce9d01f4040eb7a70b53f27286b669462ed0c1
Stefan <stepnaumovski@gmail.com>
2018-05-31 16:35:27 +0200

    update

--- archive-bookmaker.php
+++ archive-bookmaker.php
@@ -7,19 +7,6 @@
 
 <main id="main" class="category-page">
 
- <div class="three-parts post-page-area hlm-sports-widget">
-
-          <div class="post-title">
-            <h1>
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-            </h1>
-          </div>
-
-  </div>
-
   <div class="three-parts post-page-area hlm-sports-widget">
         <div class="post-content widget blog-post">
 
@@ -30,7 +17,78 @@
         <?php
       if ( have_posts() ) :
         while ( have_posts() ) : the_post();
-			include( locate_template( 'widgets/top-5-reviews/looks-1.php', false, false ) );  
+			?>
+
+
+	<li>
+		<div class="top-5-logo-wrap">
+			<div class="top-5-position-number">
+				<?php echo ($i + 1);?>
+			</div>
+
+			<div class="mobile-star-rating">
+				<?php hlm_sports_get_star_rating(get_field('overall_rating', $bookmaker_id));?>
+			</div>
+
+			<a href="<?php echo get_permalink( $bookmaker_id); ?>">
+			<div class="top-5-logo bookmaker-background-wrap-<?php echo $bookmaker_id; ?>">		 
+				 <?php $image = get_field('logo_136x44', $bookmaker_id);					                  
+						if( $image ) {?>
+							<img src="<?php  echo $image['sizes']['hlm_sports_136x44']; ?>" alt="">  							 
+						<?php } ?>											
+			</div>
+			</a>	
+		</div>
+		<div class="top-5-review-image-wrap">
+			<div class="top-5-review-image"> 
+		<?php 
+				
+				$review_img_url = get_the_post_thumbnail_url( $bookmaker_id, 'hlm_sports_232x310' ); 
+				
+				 ?>
+				 <img src="<?php   echo $review_img_url; ?>" alt="">
+			</div>
+		
+			<div class="top-5-review-content-wrap">
+				<div class="top-5-bookmaker-title">
+<a href="<?php echo get_permalink( $bookmaker_id); ?>">					
+					<?php echo get_the_title( $bookmaker_id );?>
+</a>
+				</div>
+				<div class="top-5-bonus">
+					<?php echo $bonus[$i];	?>
+				</div>
+				<div class="top-5-star-rating">
+						<?php hlm_sports_get_star_rating(get_field('overall_rating', $bookmaker_id));?>
+				</div>
+				<div class="top-5-tooltip">
+					<?php echo $tooltip[$i];	?>
+
+				</div>
+
+
+			</div>
+	<div class="bookmakers-links-wrap">			
+				<div class="top-5-review-link">
+					<a href="<?php echo get_permalink( $bookmaker_id); ?>">
+						<?php the_field('review', 'option');  ?>
+					</a>
+				</div>
+				<a href="<?php echo the_field( 'default_tracker', $bookmaker_id); ?>" target="_blank">
+					<div class="top-5-review-bet-now">
+                    	<?php the_field('bet_now', 'option');  ?>
+					</div>
+                </a>
+				<div class="top-5-rules-link">
+					<?php the_field('t&c_apply', 'option');  ?>
+				</div>
+	</div>
+		</div>
+	</li>
+
+
+
+			<?php 
         endwhile;
         //pagination
         
