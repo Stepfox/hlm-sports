3afd11e2e3f1bdeb3fb4fa7f86150e02bd07a60b
Stefan Naumovski <stefan.naumovski@stockholm.webguidepartner.com>
2018-06-13 16:55:56 +0200

    update

--- landing-page.php
+++ landing-page.php
@@ -11,153 +11,241 @@
 <!--pingback-->
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <!--stylesheet-->
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/css/bootstrap.min.css'; ?>" />
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/css/landing.css'; ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/css/bootstrap-grid.min.css'; ?>" />
+
+<!-- 
+
+https://bootsnipp.com/snippets/featured/findcond-navbar 
+https://bootsnipp.com/snippets/featured/services-section-using-bootstrap-4
+
+
+
+
+
+-->
+
+
 </head>
 
 
-<nav class="navbar navbar-default navbar-fixed-top">
+<body>
+
+
+<nav class="navbar navbar-findcond navbar-fixed-top">
     <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header page-scroll">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+            <a class="navbar-brand" href="#">Findcond</a>
         </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="navbar">
             <ul class="nav navbar-nav navbar-right">
-                <li class="hidden">
-                    <a href="#page-top"></a>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-fw fa-bell-o"></i> Bildirimler <span class="badge">0</span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">Music</span> sayfasında iletiniz beğenildi</a></li>
+                        <li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">Video</span> sayfasında iletiniz beğenildi</a></li>
+                        <li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">Game</span> sayfasında iletiniz beğenildi</a></li>
+                    </ul>
                 </li>
-                <li class="page-scroll">
-                    <a href="#portfolio">Portfolio</a>
-                </li>
-                <li class="page-scroll">
-                    <a href="#about">About</a>
-                </li>
-                <li class="page-scroll">
-                    <a href="#contact">Contact</a>
+                <li class="active"><a href="#">Ana Sayfa <span class="sr-only">(current)</span></a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Admin <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Geri bildirim</a></li>
+                        <li><a href="#">Yardım</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Ayarlar</a></li>
+                        <li><a href="#exit">Çıkış yap</a></li>
+                    </ul>
                 </li>
             </ul>
         </div>
-        <!-- /.navbar-collapse -->
     </div>
-    <!-- /.container-fluid -->
 </nav>
-<header id="page-top">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <img class="img-responsive" src="http://ironsummitmedia.github.io/startbootstrap-freelancer/img/profile.png" alt="">
-                <div class="intro-text">
-                    <span class="name">Inspired by PureCSS.io</span>
-                    <hr class="star-light">
-                    <p class="skills">Landing Page Layout</p>
-                    <p class="skills">Scroll to see the effect</p>
+
+
+
+<div id="top-element" class="container-fluid">
+
+
+<!-- Services section -->
+<div class="container">
+        <p class="h1 col-8">Top World Cup Bets</p>
+        <p class="text-left col-8 text-muted h5">Bet $10 on Harry Kane winning the Golden Boot and win $140</p>
+        <div class="container-fluid">
+            <div class="row mt-5">
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 col-xl-3">
+                    <div class="card">
+                        <div class="card-block block-1">
+                            <h3 class="card-title">Special title</h3>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="javascript:void();" title="Read more" class="read-more" >Read more<i class="fa fa-angle-double-right ml-2"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 col-xl-3">
+                    <div class="card">
+                        <div class="card-block block-2">
+                            <h3 class="card-title">Special title</h3>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="javascript:void();" title="Read more" class="read-more" >Read more<i class="fa fa-angle-double-right ml-2"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 col-xl-3">
+                    <div class="card">
+                        <div class="card-block block-3">
+                            <h3 class="card-title">Special title</h3>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="javascript:void();" title="Read more" class="read-more" >Read more<i class="fa fa-angle-double-right ml-2"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 col-xl-3">
+                    <div class="card">
+                        <div class="card-block block-3">
+                            <h3 class="card-title">Special title</h3>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="javascript:void();" title="Read more" class="read-more" >Read more<i class="fa fa-angle-double-right ml-2"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div>  
     </div>
-</header>
-<div class="content-wrapper">
-    <section class="primary" id="portfolio">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>PORTAFOLIO</h2>
-                    <hr class="star-primary">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-4">
-                    <img src="http://lorempixel.com/360/260/nature/">    
-                </div>
-                <div class="col-sm-4">
-                    <img src="http://lorempixel.com/360/260/animals/">    
-                </div>
-                <div class="col-sm-4">
-                    <img src="http://lorempixel.com/360/260/abstract/">    
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="success" id="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>About</h2>
-                    <hr class="star-light">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-lg-offset-2">
-                    <p>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
-                </div>
-                <div class="col-lg-4">
-                    <p>Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="primary" id="contact">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>Contact</h2>
-                    <hr class="star-primary">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
-                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
-                    <form name="sentMessage" id="contactForm" novalidate="">
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Name</label>
-                                <input type="text" class="form-control" placeholder="Name" id="name" required="" data-validation-required-message="Please enter your name.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Email Address</label>
-                                <input type="email" class="form-control" placeholder="Email Address" id="email" required="" data-validation-required-message="Please enter your email address.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Phone Number</label>
-                                <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required="" data-validation-required-message="Please enter your phone number.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Message</label>
-                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required="" data-validation-required-message="Please enter a message."></textarea>
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <br>
-                        <div id="success"></div>
-                        <div class="row">
-                            <div class="form-group col-xs-12">
-                                <button type="submit" class="btn btn-success btn-lg">Send</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
-    <footer class="container" style="min-height:200px; background-color:#18bc9c;color:#fff;text-align:center;padding-top:50px;">
-        INSPIRED BY PURECSS.IO © 2015 
-    </footer>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Services section -->
+<div class="container">
+    <section id="what-we-do">
+        <div class="container-fluid">
+            <h2 class="section-title mb-2 h1">What we do</h2>
+            <p class="text-center text-muted h5">Having and managing a correct marketing strategy is crucial in a fast moving market.</p>
+            <div class="row mt-5">
+                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                    <div class="card">
+                        <div class="card-block block-1">
+                            <h3 class="card-title">Special title</h3>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="javascript:void();" title="Read more" class="read-more" >Read more<i class="fa fa-angle-double-right ml-2"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                    <div class="card">
+                        <div class="card-block block-2">
+                            <h3 class="card-title">Special title</h3>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="javascript:void();" title="Read more" class="read-more" >Read more<i class="fa fa-angle-double-right ml-2"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                    <div class="card">
+                        <div class="card-block block-4">
+                            <h3 class="card-title">Special title</h3>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="javascript:void();" title="Read more" class="read-more" >Read more<i class="fa fa-angle-double-right ml-2"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+    </section>
+    <!-- /Services section -->
+</div>
+
+
+
+
+
+<div id="bonus-list" class="container">
+        <div class="container-fluid">
+            <p class="section-title mb-2 h1 text-center">What we do</p>
+             <div class="card">
+                <img class="col-sm-12 col-xs-3 col-md-3" src="http://placehold.it/350x80" alt="placehold.it/350x80" >
+                <div class="col-sm-12 col-xs-6 col-md-6">
+                    <p class="list-group-item-text"> Eu eum corpora torquatos, ne postea constituto mea, quo tale lorem facer no. Ut sed odio appetere partiendo, quo meliore salutandi ex. Vix an sanctus vivendo, sed vocibus accumsan petentium ea.                         
+                    </p>
+                </div>
+                <div class="col-sm-12 col-xs-3 col-md-3 text-center">
+                    <button type="button" class="btn btn-primary btn-lg btn-block">Vote Now!</button>
+                </div>
+            </div>
+
+             <div class="card">
+                <img class="col-sm-12 col-xs-3 col-md-3" src="http://placehold.it/350x80" alt="placehold.it/350x80" >
+                <div class="col-sm-12 col-xs-6 col-md-6">
+                    <p class="list-group-item-text"> Eu eum corpora torquatos, ne postea constituto mea, quo tale lorem facer no. Ut sed odio appetere partiendo, quo meliore salutandi ex. Vix an sanctus vivendo, sed vocibus accumsan petentium ea.                         
+                    </p>
+                </div>
+                <div class="col-sm-12 col-xs-3 col-md-3 text-center">
+                    <button type="button" class="btn btn-primary btn-lg btn-block">Vote Now!</button>
+                </div>
+            </div>
+
+             <div class="card">
+                <img class="col-sm-12 col-xs-3 col-md-3" src="http://placehold.it/350x80" alt="placehold.it/350x80" >
+                <div class="col-sm-12 col-xs-6 col-md-6">
+                    <p class="list-group-item-text"> Eu eum corpora torquatos, ne postea constituto mea, quo tale lorem facer no. Ut sed odio appetere partiendo, quo meliore salutandi ex. Vix an sanctus vivendo, sed vocibus accumsan petentium ea.                         
+                    </p>
+                </div>
+                <div class="col-sm-12 col-xs-3 col-md-3 text-center">
+                    <button type="button" class="btn btn-primary btn-lg btn-block">Vote Now!</button>
+                </div>
+            </div>
+
+
+
+        </div>
+</div>
+
+
+
+
+
+
+<div class="container text-center">
+<img class="align-self-center" src="http://placehold.it/350x80" alt="placehold.it/350x80" >
+</div>
+<div class="container text-center">
+            <a href="#">Toggle navigation</a>
+            <a href="#">Toggle navigation</a>
+            <a href="#">Toggle navigation</a>
+            <a href="#">Toggle navigation</a>
+</div>
+
+<div class="container text-center">
+    copyrightcopyrightcopyright copyright copyright copyright copyright copyright copyright copyright copyright copyright 
+</div>
+<div class="container text-center">
+<img class="align-self-center" src="http://placehold.it/350x80" alt="placehold.it/350x80" >
+</div>
+
+
+
+
+
+
+
+</body>
