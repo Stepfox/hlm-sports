16ce64b430e2ea472d1a426b000cb28ab9e179cc
Stefan <stepnaumovski@gmail.com>
2018-07-20 11:04:01 +0200

    update

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <!--pingback-->
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
 <?php wp_head(); ?>
 
 <?php $scripts = get_field('scripts', 'option'); eval( $scripts );  ?>
