1d8cee06b1c428ccde521e045ac7d23393c9a438
Stefan <stepnaumovski@gmail.com>
2018-07-30 09:01:33 +0200

    return

--- widgets/odds-widget/matches-cpt-registration.php
+++ widgets/odds-widget/matches-cpt-registration.php
@@ -152,7 +152,10 @@ add_action( 'cron_crawl_odds', 'cron_crawl_odds' );
     //     wp_schedule_event( time(), 'halfhour', 'cron_remove_past_matches' );
     // }
 
-if ($_SERVER['HTTP_HOST'] != '35.189.74.126' && $_SERVER['HTTP_HOST'] != 'http://hlm-sports-betting.local' ){
+if ($_SERVER['HTTP_HOST'] != '35.189.74.126' || $_SERVER['HTTP_HOST'] != 'http://hlm-sports-betting.local' ){
+
+    if ( ! wp_next_scheduled( 'cron_crawl_odds' ) ) {
+        wp_schedule_event( time(), 'halfhour', 'cron_crawl_odds' );
     }
 
     if ( ! wp_next_scheduled( 'cron_crawl_matches' ) ) {
@@ -162,12 +165,7 @@ if ($_SERVER['HTTP_HOST'] != '35.189.74.126' && $_SERVER['HTTP_HOST'] != 'http:/
 }
 
 
-if ($_SERVER['HTTP_HOST'] == '35.189.74.126' && $_SERVER['HTTP_HOST'] != 'http://hlm-sports-betting.local' ){
 
-    if ( ! wp_next_scheduled( 'cron_crawl_odds' ) ) {
-        wp_schedule_event( time(), 'halfhour', 'cron_crawl_odds' );
-    }
-}
 
 
 
