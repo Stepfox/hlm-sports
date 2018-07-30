2d75c9e4dc7681d1b4e15638b0e9f662bcd520f2
Stefan <stepnaumovski@gmail.com>
2018-07-30 09:20:53 +0200

    update

--- taxonomy-teams.php
+++ taxonomy-teams.php
@@ -35,30 +35,51 @@
          Upcoming Games
       </h2>
     </div>
-  <ul>
+                        <table class="matches-table">
+                          <tbody>
+
+
+
         <?php
+        $same_day_check = '414444444444444';
+        $check=0;
       if ( have_posts() ) :
         while ( have_posts() ) : the_post();
+
+                          $myDateTime = (int)get_field('start_time');
+                          $game_date = (string)date("d Y",$myDateTime);
+                          $check_game_date = (string)date("d Y",$same_day_check);                        
+                          if ( $game_date != $check_game_date ){
+                          
              ?>               
-        <li>          
+                            <tr>
+                              <td class="match-date" colspan="6">
+                                <?php echo date ('F d, Y',$myDateTime); ?>
+                              </td>
+                            </tr>
 
-            <?php experiment_5(); ?>
+<?php 
 
-        </li>
+
+
+      $same_day_check =  $myDateTime;
+                       }
+                       
+
+            experiment_7($check); 
+            $check++;
+            ?>
+
+
         <?php 
         endwhile;
                  
       endif;
     ?>
-  </ul>
-    <div class="widget-title">
-      <h2>
-         Roster
-      </h2>
-    </div>
-   <?php experiment_6($term);?>
-    </div>
-  </div>
+
+                          </tbody>
+                        </table>
+
   <div class="one-part post-page-area">
       <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Post Sidebar')): endif; ?>
   </div>
