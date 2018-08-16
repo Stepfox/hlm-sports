67e297f67d3a0ce68b839dcd9d4148c78198f666
Stefan <stepnaumovski@gmail.com>
2018-08-16 13:21:21 +0200

    update

--- header.php
+++ header.php
@@ -12,9 +12,11 @@
 
 <?php $scripts = get_field('scripts', 'option'); eval( $scripts );  ?>
 <?php 
-if ($_SERVER['HTTP_HOST'] != 'www.winningsportsbets.co.uk' || $_SERVER['HTTP_HOST'] != 'winningsportsbets.co.uk'){
+if ($_SERVER['HTTP_HOST'] == 'www.winningsportsbets.co.uk' || $_SERVER['HTTP_HOST'] == 'winningsportsbets.co.uk'){
 
-	echo '<meta name="robots" content="noindex deez">';
+	
+}else{
+	echo '<meta name="robots" content="noindex">';
 }
 
 ?>
