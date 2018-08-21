c2ba69875b229b981b0364df5ad8aeff13311d88
Stepfox <Stepfox@users.noreply.github.com>
2018-08-21 10:32:04 +0200

    update

--- front-page.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-	<main id="main">
-		<div id="full-area">
-			<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Full Widgetarea')): endif; ?>
-		</div>	
-	</main>
-<?php get_footer(); ?>
